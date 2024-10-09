a93edf2e154da3964cac925bb7ec7610bab864d1
David <cincoisc@gmail.com>
2024-10-08 18:38:01 -0700

    class forms, views, templates added

--- AnotherTodoApp/todo/models.py
+++ AnotherTodoApp/todo/models.py
@@ -2,14 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Todo(models.Model):
-    todoid = models.AutoField(primary_key=True)        
-    tododescription = models.TextField(blank=True, null=True)
-    tododone = models.BooleanField(blank=True, null=True)
+    todoid = models.AutoField(primary_key=True)
+    tododescription = models.TextField(blank=False, null=False)
+    tododone = models.BooleanField(default=False, null=False)
 
     class Meta:
         managed = False
         db_table = 'todo'
-
 
 class Users(models.Model):
     userid = models.AutoField(primary_key=True)        
--- AnotherTodoApp/todo/views.py
+++ AnotherTodoApp/todo/views.py
@@ -1,24 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 
 from django.template import loader
 from django.shortcuts import render
 
 from .models import Todo
+from .forms import TodoForm
 
 # Create your views here.
 def index(request):
     todos = Todo.objects.all()
-
-    context = {"message": "hello world", "tasks": todos}
+    context = {"tasks": todos}
     return render(request, 'todo/index/index.html', context)
 
 def create(request):
-    return HttpResponse("create views from todo")
+    form = TodoForm
+    if request.method == "POST":            
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    return redirect("todo:index")
+    
         
 def update(request, id):
     return HttpResponse(f"update views from todo - {id}")
 
 def delete(request, id):
-    return HttpResponse(f"delete views from todo - {id}")
+    task = Todo.objects.get(pk=id)
+    if task.DoesNotExist():
+        task.delete()
+        
+    return redirect("todo:index")
 
